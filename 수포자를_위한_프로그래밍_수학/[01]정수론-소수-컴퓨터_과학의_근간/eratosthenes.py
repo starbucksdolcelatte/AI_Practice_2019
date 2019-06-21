3af2d67f230c6c7d421706d0ebb9a49cce5c397d
starbucksdolcelatte <seoyoon9535@gmail.com>
2019-06-22 00:34:08 +0900

    에라토스테네스의 체'

---
+++ 수포자를_위한_프로그래밍_수학/[01]정수론-소수-컴퓨터_과학의_근간/eratosthenes.py
@@ -0,0 +1,20 @@
+def eratosthenes(n):
+    # 숫자 목록을 준비 (True는 소수임을 의미)
+    sieve = [True] * n
+
+    for i in range(2, n):
+        # i가 소수인 경우
+        if sieve[i] == True:
+            # i의 배수를 False 판정
+            # To-do
+            j = 2
+            while (j*i < n):
+                sieve[i*j] = False
+                j += 1
+            pass
+            print(sieve)
+    # 소수 목록 산출
+    return [i for i in range(2, n) if sieve[i] == True]
+
+
+print(eratosthenes(10))
