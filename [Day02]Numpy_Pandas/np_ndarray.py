92c7a8f203e5a211e0ab91a3f13e592b054c3e69
starbucksdolcelatte <seoyoon9535@gmail.com>
2019-07-04 10:38:36 +0900

    numpy ndarray practice

---
+++ [Day02]Numpy_Pandas/np_ndarray.py
@@ -0,0 +1,20 @@
+import numpy as np
+x = [[1, 2, 3], [1,3,5],[5,6,7]]
+
+# list to array
+print(np.array(x))
+
+# zero
+# parameter is tuple
+print(np.zeros((3,3)))
+
+# one
+# parameter is tuple
+print(np.ones((4,5)))
+
+# empty
+# parameter is tuple
+print(np.empty((2,3)))
+
+# np.arange (range(1,10))
+print(np.arange(1,10))
